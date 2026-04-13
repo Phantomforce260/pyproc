50b61fc33b8cb6b1c3d527bae5882dae1ef81cd3
Adrian <aakeem0920@gmail.com>
2026-04-13 12:52:38 -0400

    Client can handle disconnects from receiver

--- client.py
+++ client.py
@@ -1,32 +1,62 @@
 import socket
 import sys
+import time
+
+from hyprproc import Hypr
+from processes import ProcessManager
 
 class ProcClient:
-    def say_hello(self):
+    def __init__(self):
+        self.hypr = Hypr()
+
+    def say_hello(self) -> str:
         return "Hello from client!"
 
-    def handle_command(self, cmd: str):
-        if cmd == "sayHello":
-            return self.say_hello()
-        else:
-            return f"Unknown command: {cmd}"
+    def hypr_proc(self) -> str:
+        return "\n".join([str(c) for c in self.hypr.clients()]) + "\n"
+
+    def handle_command(self, cmd: str) -> str:
+        match cmd:
+            case "sayHello":
+                return self.say_hello()
+            case "hypr":
+                return self.hypr_proc()
+            case _:
+                return f"Unknown command: {cmd}"
+
+    def connect(self, host, port):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_socket.connect((host, port))
+        return client_socket
 
     def start_client(self, host: str, port=8008):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((host, port))
-            print(f"Connected to receiver at {host}:{port}")
-
+        while True:
             try:
+                print("Attempting to connect to server...")
+                sock = self.connect(host, port)
+                print("Connected to server")
                 while True:
-                    data = client_socket.recv(1024)
+                    data = sock.recv(1024)
                     if not data:
+                        print("Server disconnected.")
                         break
+
                     cmd = data.decode().strip()
                     print(f"Received command: {cmd}")
+
                     response = self.handle_command(cmd)
-                    client_socket.sendall(response.encode())
+                    sock.sendall(response.encode())
+            except (ConnectionRefusedError, OSError):
+                print("Server not available. Retrying in 3 seconds...")
+                time.sleep(3)
             except KeyboardInterrupt:
                 print("Client exiting...")
+                exit(0)
+            finally:
+                try:
+                    sock.close()
+                except:
+                    pass
 
 
 if __name__ == "__main__":
